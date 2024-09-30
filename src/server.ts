0d43c05cda20f664c13fdb917b070a9e12a86297
ronaldAlhazov <ronalda@moveohls.com>
2024-09-30 17:20:13 +0300

    creating the app

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Example route
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World ");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
